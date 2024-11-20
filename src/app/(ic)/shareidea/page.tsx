fc796e04c88f7c989b91354328046f1a45221c71
AmeenCodes01 <masaqib5@gmail.com>
2024-11-20 15:42:18 +0000

    create idea + react-hook-form

--- src/app/(ic)/shareidea/page.tsx
+++ src/app/(ic)/shareidea/page.tsx
@@ -1,135 +1,186 @@
+"use client";
+
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {Textarea} from "@/components/ui/textarea";
-import {fetchMutation} from "convex/nextjs";
 
+import {zodResolver} from "@hookform/resolvers/zod";
+import {z} from "zod";
 import Link from "next/link";
 import {api} from "../../../../convex/_generated/api";
 import {revalidatePath} from "next/cache";
 import {FormEvent} from "react";
+import {useForm} from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {useMutation} from "convex/react";
+import {useRouter} from "next/navigation";
+
+const formSchema = z.object({
+  title: z.string().min(1, "Cannot be left blank"),
+  shortDesc: z.string().min(1, "Cannot be left blank"),
+  description: z.string().min(1, "Cannot be left blank"),
+  lookingFor: z.string().min(1, "Cannot be left blank"),
+  meetingFormat: z.string().min(1, "Cannot be left blank"),
+  limit: z.string(),
+});
 
 function ShareIdea() {
-  async function createIdea(event: FormEvent<HTMLFormElement>) {
-    "use server";
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    // Helper function to validate form data
-    const getField = (fieldName: string, required: boolean = true): string => {
-      const value = formData?.get(fieldName) as string;
-      if (
-        required &&
-        (value === null || (typeof value === "string" && value.trim() === ""))
-      ) {
-        throw new Error(`Field '${fieldName}' is required.`);
-      }
-      return value;
-    };
-    console.log("hello, trying to send");
+  const createIdea = useMutation(api.ideas.create);
+  const router = useRouter();
 
-    try {
-      // Validate and get the fields from formData
-      const title = getField("title");
-      const shortDesc = getField("shortDesc");
-      const description = getField("description");
-      const lookingFor = getField("lookingFor");
-      const meetingFormat = getField("meetingFormat");
-
-      // Safely handle 'limit' as a number, with a fallback of 3
-      const limitValue = formData.get("limit");
-      const limit =
-        limitValue !== null && !isNaN(Number(limitValue))
-          ? parseInt(limitValue as string)
-          : 3;
-
-      // Send the request
-      await fetchMutation(api.ideas.create, {
-        title,
-        shortDesc,
-        description,
-        lookingFor,
-        meetingFormat,
-        limit,
-      });
-
-      // Revalidate the path after successful creation
-      revalidatePath("/homefeed");
-    } catch (error) {
-      // Handle errors (form validation or API errors)
-      console.error("Error creating idea:", error);
-      // Here you could show an alert or return an error response depending on your application flow.
-    }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      shortDesc: "",
+      description: "",
+      lookingFor: "",
+      meetingFormat: "",
+      limit: "1",
+    },
+  });
+  const {
+    formState: {errors, isSubmitting},
+  } = form;
+  console.log(isSubmitting, "isSubmittin");
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    const newVal = {...values, limit: parseInt(values.limit)};
+    createIdea(newVal);
+    console.log(values, " V A L U E S FROM ZOD");
+    router.push("/homefeed");
   }
 
   return (
-    <div className="justify-center items-center flex  flex-col w-full  h-full  ">
-      <form
-        className="flex flex-col gap-4  md:gap-6  items-center self-center w-[90%] md:w-[80%]  "
-        onSubmit={createIdea}>
-        <div className="grid w-full max-w-sm items-center gap-1">
-          <Label>title</Label>
-          <Input
-            type="text"
-            placeholder=" Idea title i.e Virtual Card Maker "
-            className="w-full"
-            name="title"
-          />
-        </div>
-        {/* <div className="grid w-full max-w-sm items-center gap-1">
-          <Label>type</Label>
+    <Form {...form}>
+      {/* <div className="justify-center items-center flex  flex-col w-full  h-full  "> */}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 ">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder=" Idea title i.e Virtual Card Maker "
+                  className="w-full"
+                  {...field}
+                />
+              </FormControl>
 
-          <Input  2
-            type="text"
-            placeholder="personal, mobile app"
-            className="w-full"
-          />
-        </div> */}
-        <div className="grid w-full max-w-sm items-center gap-1">
-          <Label className="text-xs ">Would love collaborating with:</Label>
-          <Input
-            type="text"
-            name="collab"
-            placeholder="artist / web dev / 3d / psychology etc"
-          />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1">
-          <Label className="text-xs ">Meeting format</Label>
-          <Input
-            type="text"
-            name="meetingFormat"
-            placeholder="weekly 3 hrs / bi-weekly 2hrs /  "
-          />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1">
-          <Label className="text-xs ">No. of Team members</Label>
-          <Input type="number" name="limit" placeholder="1" min={1} max={6} />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1">
-          <Label className="text-xs ">short description:</Label>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="shortDesc"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>short description</FormLabel>
+              <FormControl>
+                <Input placeholder="short description 1-2 lines" {...field} />
+              </FormControl>
 
-          <Input type="text" name="shortDesc" placeholder="short description" />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-4">
-          <Textarea
-            placeholder="Share more about your idea, from which school you'd like teammates to be or the skills you need help with etc.
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="lookingFor"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="text-xs font-light">
+                Would love collaborating with:
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  {...field}
+                  placeholder="artist / web dev / 3d / psychology etc"
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="meetingFormat"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Meeting Format</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="weekly 3 hrs / bi-weekly 2hrs /  "
+                  {...field}
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="limit"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>limit</FormLabel>
+              <FormControl>
+                <Input type="number" min={1} max={6} {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel> description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Share more about your idea, from which school you'd like teammates to be or the skills you need help with etc.
              It is completely fine to say `I'll disclose more about my idea upon meeting.`
             "
-            name="description"
-            className=" px-2 min-h-32  border-[1.5px] "
-          />
-        </div>
-        {/* <Input
-          type="text"
-          placeholder="anything"
-          className="placeholder:text-xs placeholder:text-wrap placeholder:py-2"
-        /> */}
-        {/* <Link href={"/homefeed"} className="w-full flex max-w-sm mt-4"> */}
-        <Button className="w-full " type="submit">
-          Share Idea
+                  {...field}
+                  className=" px-2 min-h-32  border-[1.5px] "
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-full " type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Sharing idea..." : " Share Idea"}{" "}
         </Button>
         {/* </Link> */}
       </form>
-    </div>
+      {/* </div> */}
+    </Form>
   );
 }
 
